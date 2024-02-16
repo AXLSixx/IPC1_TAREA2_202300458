b6ea06b00ea25f3eb79e8da1c2ba0349df02e811
AXLSixx <159860422+AXLSixx@users.noreply.github.com>
2024-02-15 19:26:32 -0600

    Add files via upload

---
+++ TAREA2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea2;
+
+/**
+ *
+ * @author Axel
+ */
+public class TAREA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Axel Enrique Salan Salguero");
+    }   System.out.println("202300458")
+            
+}
